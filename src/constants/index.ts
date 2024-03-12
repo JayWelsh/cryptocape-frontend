f0b1b34dd87496237d6216ec40ebdf7468e332fc
Jay Welsh <thealchemicalopus@gmail.com>
2024-03-12 11:41:09 +0200

    test(workflow): test caching behaviour

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 // API
 export const API_ENDPOINT = "https://api.cryptocape.com";
+export const API_ENDPOINT_LOCAL = "http://localhost:8420";
+// export const API_ENDPOINT = "http://localhost:8420";
